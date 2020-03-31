047d59788a0a9004112de1a84d7657a9f583662a
Aye Miranda <ayemiranda@Ayes-MacBook-Air.local>
2020-03-31 18:33:29 +0200

    update

--- guess the correlation game.R
+++ guess the correlation game.R
@@ -1,15 +1,19 @@
-library(ggplot2)
+# Inspired by http://guessthecorrelation.com/
 
+library(ggplot2)
 
 guess <- function() {
   x = runif(200)
   q <- data.frame(x = x,
                   y = abs(x + (runif(200) -runif(200))/(runif(1)*3) ))
-  print(ggplot(q, aes(x, y)) + 
+  q$y <- q$y/max(q$y)
+  show(ggplot(q, aes(x, y)) + 
           geom_point(shape = 18, colour = "black", fill = "black", stroke = 2)+
-          theme_bw() + xlab("") + ylab("") 
+          theme_bw() + xlab("") + ylab("") + 
+          theme(panel.grid = element_blank(), axis.text = element_blank(), axis.ticks = element_blank())
         )
   return(cor(q$x,q$y))
+  
 }
 guess()
 
